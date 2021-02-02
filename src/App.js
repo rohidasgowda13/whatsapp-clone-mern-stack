baf786f6c545e1e6e978ea2d7d15a71f326fb29e
Rohidas Gowda <37271044+rohidasgowda13@users.noreply.github.com>
2021-02-02 17:57:26 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import Chat from './Chat';
+import Sidebar from './Sidebar';
+import Pusher from 'pusher-js';
+import axios from './axios';
+
+
+function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    axios.get('/messages/sync')
+    .then(response => {
+      setMessages(response.data)
+    });
+  }, [])
+
+  useEffect(() => {
+    const pusher = new Pusher('0f20438650bdb8e593e0', {
+      cluster: 'ap2'
+    });
+
+    var channel = pusher.subscribe('messages');
+    channel.bind('inserted', (newMessage) => {
+      setMessages([...messages, newMessage])
+    });
+
+    return ()=>{
+      channel.unbind_all();
+      channel.unsubscribe();
+    };
+  }, [messages]);
+
+  console.log(messages)
+
+  return (
+    <div className="app">
+      <div className="app__body">
+        {/* sidebar */}
+        <Sidebar />
+        {/* chat component */}
+        <Chat messages={messages}/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAI25utxJsBFSgknURUqZrl_udKidQ26BQ",
+    authDomain: "whatsapp-mern-6a4a4.firebaseapp.com",
+    projectId: "whatsapp-mern-6a4a4",
+    storageBucket: "whatsapp-mern-6a4a4.appspot.com",
+    messagingSenderId: "1057677966227",
+    appId: "1:1057677966227:web:6a9094852b7a4fb01ac666"
+  };
